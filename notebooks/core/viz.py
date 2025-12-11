11c8fd6dde4cc4042e8a4946e3a1b8a2ba02b96c
Deboparna Banerjee <bdebo236@gmail.com>
2025-12-11 03:23:02 +0000

    4plot traj

--- notebooks/core/viz.py
+++ notebooks/core/viz.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 def plot_trajectory(global_poses, title="Camera Trajectory", save_path="../results/trajectory.png"):
     xs, ys, zs = [], [], []
 
@@ -11,33 +10,46 @@ def plot_trajectory(global_poses, title="Camera Trajectory", save_path="../resul
         ys.append(cam_pos[1])
         zs.append(cam_pos[2])
 
-    fig = plt.figure(figsize=(8, 6))
-    ax = fig.add_subplot(111, projection="3d")
+    xs = np.array(xs)
+    ys = np.array(ys)
+    zs = np.array(zs)
 
-    ax.plot(xs, ys, zs, "-o", markersize=2)
-    ax.set_title(title)
+    fig = plt.figure(figsize=(14, 10))
 
-    ax.set_xlabel("X")
-    ax.set_ylabel("Y")
-    ax.set_zlabel("Z")
+    # -------------------------
+    # 1. Main 3D view
+    # -------------------------
+    ax1 = fig.add_subplot(2, 2, 1, projection="3d")
+    ax1.plot(xs, ys, zs, "-o", markersize=2)
+    ax1.set_title(title + " (3D)")
+    ax1.set_xlabel("X"); ax1.set_ylabel("Y"); ax1.set_zlabel("Z")
 
-    plt.tight_layout()
-    plt.savefig(save_path, dpi=200)
-    plt.close()
+    # -------------------------
+    # 2. XY projection (top-down)
+    # -------------------------
+    ax2 = fig.add_subplot(2, 2, 2)
+    ax2.plot(xs, ys, "-o", markersize=2)
+    ax2.set_aspect("equal", adjustable="box")
+    ax2.set_xlabel("X"); ax2.set_ylabel("Y")
+    ax2.set_title("XY Projection (Top-down)")
 
+    # -------------------------
+    # 3. XZ projection
+    # -------------------------
+    ax3 = fig.add_subplot(2, 2, 3)
+    ax3.plot(xs, zs, "-o", markersize=2)
+    ax3.set_aspect("equal", adjustable="box")
+    ax3.set_xlabel("X"); ax3.set_ylabel("Z")
+    ax3.set_title("XZ Projection")
 
-def plot_points_3d(points, title="3D Points", save_path="../results/points_3d.png"):
-    xs, ys, zs = points[:, 0], points[:, 1], points[:, 2]
-
-    fig = plt.figure(figsize=(7, 6))
-    ax = fig.add_subplot(111, projection="3d")
-
-    ax.scatter(xs, ys, zs, s=1)
-    ax.set_title(title)
-
-    ax.set_xlabel("X")
-    ax.set_ylabel("Y")
-    ax.set_zlabel("Z")
+    # -------------------------
+    # 4. YZ projection
+    # -------------------------
+    ax4 = fig.add_subplot(2, 2, 4)
+    ax4.plot(ys, zs, "-o", markersize=2)
+    ax4.set_aspect("equal", adjustable="box")
+    ax4.set_xlabel("Y"); ax4.set_ylabel("Z")
+    ax4.set_title("YZ Projection")
 
     plt.tight_layout()
     plt.savefig(save_path, dpi=200)
