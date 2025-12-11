5a24b5df54e19e0ae732c5d585ded9f5159cdf5a
Deboparna Banerjee <bdebo236@gmail.com>
2025-12-11 21:54:33 +0000

    3d plot same scale

--- notebooks/core/viz.py
+++ notebooks/core/viz.py
@@ -1,6 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def set_axes_equal(ax):
+    x_limits = ax.get_xlim3d()
+    y_limits = ax.get_ylim3d()
+    z_limits = ax.get_zlim3d()
+
+    x_range = x_limits[1] - x_limits[0]
+    y_range = y_limits[1] - y_limits[0]
+    z_range = z_limits[1] - z_limits[0]
+
+    max_range = max(x_range, y_range, z_range)
+
+    x_mid = np.mean(x_limits)
+    y_mid = np.mean(y_limits)
+    z_mid = np.mean(z_limits)
+
+    ax.set_xlim3d([x_mid - max_range/2, x_mid + max_range/2])
+    ax.set_ylim3d([y_mid - max_range/2, y_mid + max_range/2])
+    ax.set_zlim3d([z_mid - max_range/2, z_mid + max_range/2])
+
+
 def plot_trajectory(global_poses, title="Camera Trajectory", save_path="../results/trajectory.png"):
     xs, ys, zs = [], [], []
 
@@ -21,30 +41,25 @@ def plot_trajectory(global_poses, title="Camera Trajectory", save_path="../resul
     # -------------------------
     ax1 = fig.add_subplot(2, 2, 1, projection="3d")
     ax1.plot(xs, ys, zs, "-o", markersize=2)
+    set_axes_equal(ax1)
     ax1.set_title(title + " (3D)")
-    ax1.set_xlabel("X"); ax1.set_ylabel("Y"); ax1.set_zlabel("Z")
+    ax1.set_xlabel("X")
+    ax1.set_ylabel("Y")
+    ax1.set_zlabel("Z")
 
-    # -------------------------
-    # 2. XY projection (top-down)
-    # -------------------------
+    # projections remain unchanged...
     ax2 = fig.add_subplot(2, 2, 2)
     ax2.plot(xs, ys, "-o", markersize=2)
     ax2.set_aspect("equal", adjustable="box")
     ax2.set_xlabel("X"); ax2.set_ylabel("Y")
     ax2.set_title("XY Projection (Top-down)")
 
-    # -------------------------
-    # 3. XZ projection
-    # -------------------------
     ax3 = fig.add_subplot(2, 2, 3)
     ax3.plot(xs, zs, "-o", markersize=2)
     ax3.set_aspect("equal", adjustable="box")
     ax3.set_xlabel("X"); ax3.set_ylabel("Z")
     ax3.set_title("XZ Projection")
 
-    # -------------------------
-    # 4. YZ projection
-    # -------------------------
     ax4 = fig.add_subplot(2, 2, 4)
     ax4.plot(ys, zs, "-o", markersize=2)
     ax4.set_aspect("equal", adjustable="box")
